cb7924a47ed2df0411e99971b0f96c5db1db981c
Antonio Martínez <a.martinezmarin1@um.es>
2022-10-18 21:43:36 +0200

    Segunda parte del registro prototipo, falta añadir drag and drop, pulir las fotos y chequear el diseño

--- appPhotos/src/ventanas/Login.java
+++ appPhotos/src/ventanas/Login.java
@@ -43,6 +43,7 @@ public class Login {
 					Login window = new Login();
 					window.frame.setVisible(true);
 					window.frame.getRootPane().requestFocus(false);
+//					window.frame.setFocusableWindowState(false);
 
 				} catch (Exception e) {
 					e.printStackTrace();
