62a387673965a4041dc5571424ff9f9eac2e7c45
Bekario <adri2002.pardo@gmail.com>
2022-10-18 19:49:29 +0200

    Ventana registro avance

--- appPhotos/src/ventanas/Register.java
+++ appPhotos/src/ventanas/Register.java
@@ -3,23 +3,25 @@ package ventanas;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
 
 import com.formdev.flatlaf.intellijthemes.FlatMonokaiProIJTheme;
-
+import java.awt.Toolkit;
 import java.awt.GridBagLayout;
-import javax.swing.JLabel;
 import java.awt.GridBagConstraints;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import java.awt.Insets;
 import java.awt.Font;
 import java.awt.Color;
-import java.awt.Insets;
-
+import java.awt.Cursor;
 import javax.swing.JTextField;
 
-public class Register extends JFrame {
 
-	private JPanel contentPane;
+public class Register {
+
+	private JFrame frame;
+	private JButton botonRegister;
 	private JTextField textField;
 	private JTextField textField_1;
 	private JTextField textField_2;
@@ -34,8 +36,10 @@ public class Register extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					Register frame = new Register();
-					frame.setVisible(true);
+					Register window = new Register();
+					window.frame.setVisible(true);
+					window.frame.getRootPane().requestFocus(false);
+
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -44,75 +48,92 @@ public class Register extends JFrame {
 	}
 
 	/**
-	 * Create the frame.
+	 * Create the application.
 	 */
 	public Register() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 600);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		initialize();
+	}
 
-		setContentPane(contentPane);
-		GridBagLayout gbl_contentPane = new GridBagLayout();
-		gbl_contentPane.columnWidths = new int[]{50, 0, 50, 0};
-		gbl_contentPane.rowHeights = new int[]{15, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-		gbl_contentPane.columnWeights = new double[]{0.0, 1.0, 0.0, Double.MIN_VALUE};
-		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
-		contentPane.setLayout(gbl_contentPane);
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setIconImage(Toolkit.getDefaultToolkit().getImage(Register.class.getResource("/imagenes/camara-de-fotos.png")));
+		frame.setBounds(100, 100, 450, 600);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{50, 35, 0, 35, 50, 0};
+		gridBagLayout.rowHeights = new int[]{15, 0, 25, 0, 0, 5, 0, 10, 0, 0, 0, 0};
+		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 1.0, 0.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		frame.getContentPane().setLayout(gridBagLayout);
 		
 		JLabel nombreApp = new JLabel("appPhotos");
 		nombreApp.setForeground(new Color(233, 233, 233));
+		nombreApp.setIcon(null);
 		nombreApp.setFont(new Font("Serif", Font.BOLD, 40));
 		GridBagConstraints gbc_nombreApp = new GridBagConstraints();
 		gbc_nombreApp.insets = new Insets(0, 0, 5, 5);
-		gbc_nombreApp.gridx = 1;
+		gbc_nombreApp.gridx = 2;
 		gbc_nombreApp.gridy = 1;
-		contentPane.add(nombreApp, gbc_nombreApp);
+		frame.getContentPane().add(nombreApp, gbc_nombreApp);
 		
 		textField = new JTextField();
 		GridBagConstraints gbc_textField = new GridBagConstraints();
 		gbc_textField.insets = new Insets(0, 0, 5, 5);
 		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
-		gbc_textField.gridx = 1;
-		gbc_textField.gridy = 4;
-		contentPane.add(textField, gbc_textField);
+		gbc_textField.gridx = 2;
+		gbc_textField.gridy = 3;
+		frame.getContentPane().add(textField, gbc_textField);
 		textField.setColumns(10);
 		
 		textField_1 = new JTextField();
 		GridBagConstraints gbc_textField_1 = new GridBagConstraints();
 		gbc_textField_1.insets = new Insets(0, 0, 5, 5);
 		gbc_textField_1.fill = GridBagConstraints.HORIZONTAL;
-		gbc_textField_1.gridx = 1;
-		gbc_textField_1.gridy = 6;
-		contentPane.add(textField_1, gbc_textField_1);
+		gbc_textField_1.gridx = 2;
+		gbc_textField_1.gridy = 4;
+		frame.getContentPane().add(textField_1, gbc_textField_1);
 		textField_1.setColumns(10);
 		
 		textField_2 = new JTextField();
 		GridBagConstraints gbc_textField_2 = new GridBagConstraints();
 		gbc_textField_2.insets = new Insets(0, 0, 5, 5);
 		gbc_textField_2.fill = GridBagConstraints.HORIZONTAL;
-		gbc_textField_2.gridx = 1;
-		gbc_textField_2.gridy = 8;
-		contentPane.add(textField_2, gbc_textField_2);
+		gbc_textField_2.gridx = 2;
+		gbc_textField_2.gridy = 5;
+		frame.getContentPane().add(textField_2, gbc_textField_2);
 		textField_2.setColumns(10);
 		
 		textField_3 = new JTextField();
 		GridBagConstraints gbc_textField_3 = new GridBagConstraints();
 		gbc_textField_3.insets = new Insets(0, 0, 5, 5);
 		gbc_textField_3.fill = GridBagConstraints.HORIZONTAL;
-		gbc_textField_3.gridx = 1;
-		gbc_textField_3.gridy = 10;
-		contentPane.add(textField_3, gbc_textField_3);
+		gbc_textField_3.gridx = 2;
+		gbc_textField_3.gridy = 6;
+		frame.getContentPane().add(textField_3, gbc_textField_3);
 		textField_3.setColumns(10);
 		
 		textField_4 = new JTextField();
 		GridBagConstraints gbc_textField_4 = new GridBagConstraints();
-		gbc_textField_4.insets = new Insets(0, 0, 0, 5);
+		gbc_textField_4.insets = new Insets(0, 0, 5, 5);
 		gbc_textField_4.fill = GridBagConstraints.HORIZONTAL;
-		gbc_textField_4.gridx = 1;
-		gbc_textField_4.gridy = 12;
-		contentPane.add(textField_4, gbc_textField_4);
+		gbc_textField_4.gridx = 2;
+		gbc_textField_4.gridy = 7;
+		frame.getContentPane().add(textField_4, gbc_textField_4);
 		textField_4.setColumns(10);
+		
+		botonRegister = new JButton("");
+		botonRegister.setContentAreaFilled(false);
+		botonRegister.setBorderPainted(false);
+		botonRegister.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		botonRegister.setIcon(new ImageIcon(Register.class.getResource("/imagenes/button (3).png")));
+		GridBagConstraints gbc_botonRegister = new GridBagConstraints();
+		gbc_botonRegister.insets = new Insets(0, 0, 5, 5);
+		gbc_botonRegister.gridx = 2;
+		gbc_botonRegister.gridy = 9;
+		frame.getContentPane().add(botonRegister, gbc_botonRegister);
 	}
 
 }
