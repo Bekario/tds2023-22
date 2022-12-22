bd813e67e595cf7983d471ff8931fbe862498625
Antonio Martínez <a.martinezmarin1@um.es>
2022-12-22 11:18:35 +0100

    paquete main + lanzador

---
+++ appPhotos/src/main/Lanzador.java
@@ -0,0 +1,23 @@
+package main;
+
+import java.awt.EventQueue;
+
+import com.formdev.flatlaf.intellijthemes.FlatMonokaiProIJTheme;
+
+import ventanas.Login;
+
+public class Lanzador {
+	public static void main(final String[] args){
+		FlatMonokaiProIJTheme.setup();
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Login ventana = new Login();
+					ventana.mostrarVentana();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+}
