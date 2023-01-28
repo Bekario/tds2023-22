b90785169d532063385abae3b37b202142e79014
Antonio Martínez <a.martinezmarin1@um.es>
2023-01-28 13:45:55 +0100

    Incorporado paquete de tests

---
+++ appPhotos/src/pruebas/PersistenciaTest.java
@@ -0,0 +1,14 @@
+package pruebas;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PersistenciaTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
