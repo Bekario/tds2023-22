f12f7213df36e5108b8d91e0e620c67115383288
Antonio Martínez <a.martinezmarin1@um.es>
2023-01-28 17:15:43 +0100

    Test usuarioDAO terminado

--- appPhotos/src/pruebas/PersistenciaTest.java
+++ appPhotos/src/pruebas/PersistenciaTest.java
@@ -17,27 +17,40 @@ import persistencia.TDSFactoriaDAO;
 
 public class PersistenciaTest {
 	private static TDSFactoriaDAO factoria;
+	private static Usuario usuario;
 	
 	@BeforeClass
 	public static void prepararTests() {
 		try {
 			factoria = (TDSFactoriaDAO) TDSFactoriaDAO.getInstancia();
 		} catch (DAOException e) {}
+		
+		//Iniciamos objetos comunes para los tests
+		usuario = new Usuario("pepe", "1234", "pepepepe@gmail.com", "Pepe Pepito Pepe", LocalDate.now(), "foto", "Hola soy pepe");
+		
 		System.out.println("Tests preparados.");
 	}
 	
 	@Test
 	public void testUsuarioDAO() {
 		AdaptadorUsuarioTDS u = (AdaptadorUsuarioTDS) factoria.getUsuarioDAO();
-		Usuario user = new Usuario("pepe", "1234", "pepepepe@gmail.com", "Pepe Pepito Pepe", LocalDate.now(), "foto", "Hola soy pepe");
 		
 		//Registramos un usuario
-		u.registrarUsuario(user);
+		u.registrarUsuario(usuario);
 		
 		//Recuperamos el usuario
-		Usuario recuperado = u.recuperarUsuario(user.getCodigo());
+		Usuario recuperado = u.recuperarUsuario(usuario.getCodigo());
 		
-
+		//Comprobamos que todos los campos coincidan
+		assertEquals("El usuario no coincide", usuario.getUsuario(), recuperado.getUsuario());
+		assertEquals("La contraseña no coincide", usuario.getContraseña(), recuperado.getContraseña());
+		assertEquals("El email no coincide", usuario.getEmail(), recuperado.getEmail());
+		assertEquals("El nombre completo no coincide", usuario.getNombreCompleto(), recuperado.getNombreCompleto());
+		assertEquals("La fecha de nacimiento no coincide", usuario.getFechaNacimiento(), recuperado.getFechaNacimiento());
+		assertEquals("La imagen de perfil no coincide", usuario.getPerfil(), recuperado.getPerfil());
+		assertEquals("La descripcion no coincide", usuario.getDescripcion(), recuperado.getDescripcion());
+		
+		System.out.println("Test basico UsuarioDAO superado!");
 	}
 /*	
 	@Test
