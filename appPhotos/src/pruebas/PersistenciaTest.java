fe32a966dc3cf7776d5bf3ff8a2c7647f80c5e46
Antonio Martínez <a.martinezmarin1@um.es>
2023-01-28 15:53:44 +0100

    avance tests

--- appPhotos/src/pruebas/PersistenciaTest.java
+++ appPhotos/src/pruebas/PersistenciaTest.java
@@ -4,11 +4,41 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class PersistenciaTest {
+import persistencia.DAOException;
+import persistencia.FactoriaDAO;
+import persistencia.TDSFactoriaDAO;
 
+public class PersistenciaTest {
+	FactoriaDAO factoria;
+	
+	public void prepararTests() {
+		try {
+			factoria = TDSFactoriaDAO.getInstancia();
+		} catch (DAOException e) {}
+	}
+	
 	@Test
-	public void test() {
+	public void testUsuarioDAO() {
+
+	}
+	
+	@Test
+	public void testComentarioDAO() {
 		fail("Not yet implemented");
 	}
-
+	
+	@Test
+	public void testFotoDAO() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testAlbumDAO() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testNotificacionDAO() {
+		fail("Not yet implemented");
+	}
 }
