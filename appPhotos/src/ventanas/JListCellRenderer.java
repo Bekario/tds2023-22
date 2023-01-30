aa350e6aab52efb19a9fd500955df5150f0694e4
Bekario <adri2002.pardo@gmail.com>
2023-01-30 23:24:30 +0100

    Intento de ListRender

---
+++ appPhotos/src/ventanas/JListCellRenderer.java
@@ -0,0 +1,52 @@
+package ventanas;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.time.LocalDate;
+
+import javax.swing.DefaultListCellRenderer;
+import javax.swing.ImageIcon;
+import javax.swing.ListCellRenderer;
+import javax.swing.UIManager;
+
+import modelo.Usuario;
+
+public class JListCellRenderer extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ListCellRenderer<? super Usuario> createListRenderer() {
+		return new DefaultListCellRenderer() {
+			/**
+			 * 
+			 */
+			private static final long serialVersionUID = 1L;
+			private Color background = new Color(0, 100, 255, 15);
+			private Color defaultBackground = (Color) UIManager.get("List.background");
+
+			@Override
+			public Component getListCellRendererComponent(JList<?> list, Object value, int index, boolean isSelected,
+					boolean cellHasFocus) {
+				Component c = super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+				if (c instanceof JLabel) {
+					JLabel label = (JLabel) c;
+					Usuario usuario = (Usuario) value;
+					label.setIcon(new ImageIcon(Home.class.getResource(usuario.getPerfil())));
+					label.setText(String.format("%s [%s]", usuario.getUsuario(), LocalDate.now().toString()));
+					if (!isSelected) {
+						label.setBackground(index % 2 == 0 ? background : defaultBackground);
+					}
+				}
+				return c;
+			}
+		};
+
+	}
+}
--- appPhotos/src/ventanas/PanelBuscar.java
+++ appPhotos/src/ventanas/PanelBuscar.java
@@ -7,6 +7,7 @@ import java.awt.GridBagConstraints;
 import java.awt.Font;
 import javax.swing.JTextField;
 import java.awt.Insets;
+import javax.swing.ImageIcon;
 
 public class PanelBuscar extends JPanel {
 	private JTextField textField;
@@ -16,15 +17,16 @@ public class PanelBuscar extends JPanel {
 	 */
 	public PanelBuscar() {
 		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[]{5, 0, 5, 0};
-		gridBagLayout.rowHeights = new int[]{0, 0, 0, 0};
-		gridBagLayout.columnWeights = new double[]{0.0, 1.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.columnWidths = new int[]{5, 0, 40, 5, 0};
+		gridBagLayout.rowHeights = new int[]{0, 0, 40, 0};
+		gridBagLayout.columnWeights = new double[]{0.0, 1.0, 0.0, 0.0, Double.MIN_VALUE};
 		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
 		setLayout(gridBagLayout);
 		
 		JLabel lblNewLabel = new JLabel("Búsqueda");
 		lblNewLabel.setFont(new Font("Segoe UI", Font.BOLD, 19));
 		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+		gbc_lblNewLabel.gridwidth = 2;
 		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
 		gbc_lblNewLabel.anchor = GridBagConstraints.SOUTH;
 		gbc_lblNewLabel.gridx = 1;
