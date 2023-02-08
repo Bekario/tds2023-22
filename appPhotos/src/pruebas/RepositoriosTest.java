2f7ff464f7b39ca44965f125694637c4b771d5d0
Bekario <adri2002.pardo@gmail.com>
2023-02-08 20:25:24 +0100

    error?

--- appPhotos/src/pruebas/RepositoriosTest.java
+++ appPhotos/src/pruebas/RepositoriosTest.java
@@ -163,6 +163,7 @@ public class RepositoriosTest {
 		
 		//Volvemos a añadir al usuario 2 para que no haya problemas con otros tests
 		repositorioP.addPublicacion(p2);
+		
 		assertEquals("El usuario no se recupera correctamente con el codigo cuando no existe.", null, aux);
 		
 	}
