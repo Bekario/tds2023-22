2ba0c774f4e909271ce4e57783040812c3b81a32
Bekario <adri2002.pardo@gmail.com>
2023-04-18 23:31:38 +0200

    comentario fix

--- appPhotos/src/ventanas/PanelListaUsuarios.java
+++ appPhotos/src/ventanas/PanelListaUsuarios.java
@@ -16,6 +16,8 @@ public class PanelListaUsuarios extends JPanel {
 	
 	private static final long serialVersionUID = 1L;
 	private int y;
+	private PanelUsuario panelUsuario;
+	
 	private ArrayList<PanelUsuario> paneles;
 	private Home home;
 	
@@ -34,31 +36,31 @@ public class PanelListaUsuarios extends JPanel {
 	private void crearPanel() {
 		GridBagLayout gridBagLayout = new GridBagLayout();
 		gridBagLayout.columnWidths = new int[]{0, 0};
-		gridBagLayout.rowHeights = new int[]{0, 0, 0, 0};
+		gridBagLayout.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0};
 		gridBagLayout.columnWeights = new double[]{1.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		setLayout(gridBagLayout);
 	}
 	
 	public void addUsuario(Usuario usuario) {
-		PanelUsuario panelUsuario = new PanelUsuario(usuario);
+		panelUsuario = new PanelUsuario(usuario);
 		GridBagConstraints gbc_panelUsuario = new GridBagConstraints();
 		gbc_panelUsuario.insets = new Insets(0, 0, 5, 0);
 		gbc_panelUsuario.fill = GridBagConstraints.BOTH;
 		gbc_panelUsuario.gridx = 0;
 		gbc_panelUsuario.gridy = y;
-		y+=1;
 		add(panelUsuario, gbc_panelUsuario);
 		paneles.add(panelUsuario);
+		y++;
 		
 		addManejadorClickUsuario(panelUsuario, usuario);
 	}
 	
 	public void addListaUsuario(List<Usuario> usuarios) {
-		for (Usuario usuario : usuarios) {
-			addUsuario(usuario);
-		}
+		usuarios.stream()
+				.forEach(u -> addUsuario(u));
 	}
+
 	
 	public void quitarUsuarios() {
 		removeAll();
