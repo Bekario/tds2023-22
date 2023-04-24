37a8959a87011e5cac3551c1b35d170fe4bec43d
Antonio Martínez <a.martinezmarin1@um.es>
2023-04-24 20:09:48 +0200

    COLORES

---
+++ appPhotos/src/ventanas/Colores.java
@@ -0,0 +1,8 @@
+package ventanas;
+
+import java.awt.Color;
+
+public class Colores {
+	public static Color NARANJA = new Color(240, 175, 36);
+	public static Color NARANJA_CLARO = new Color(247, 211, 131);
+}
